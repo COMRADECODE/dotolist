dd597911b80da269ec41bbf3f36385d1ada670a1
dhousik <dhousiksiva1997@gmail.com>
2023-08-26 07:42:25 +0530

    Creating pop-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatDialog, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import { PopUpComponent } from './pop-up/pop-up.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dotolist';
+  selectedColor: string = 'transparent';
+  constructor(public dialog: MatDialog) {}
+  changeColor(color: string) {
+    this.selectedColor = color;
+  }
+  openDialog(): void {
+    // const dialogRef = this.dialog.open(PopUpComponent, {
+    //   width: '500px',
+    //   height:'500px'
+    // }); 
+    const dialogRef: MatDialogRef<PopUpComponent> = this.dialog.open(PopUpComponent,{
+      disableClose: true
+    });
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log('The dialog was closed');
+      console.log(result);//returns undefined
+    });
+  } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatDialog, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { PopUpComponent } from './pop-up/pop-up.component';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopUpComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +24,10 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatSelectModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatDialogModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent],
